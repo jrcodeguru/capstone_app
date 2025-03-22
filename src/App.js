5cbcd7d0fab86e5d47c3c00b1602d2a36cc0d74a
jrcodeguru <jrcodeguru@gmail.com>
2025-03-22 22:59:13 +0530

    Updated index.html and App.js, added new components and images

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Learn React</div>;
+  return (
+    <div>
+      <h1>Meta Frontend Developer Capstone Starter Files</h1>
+    </div>
+  );
 }
 
 export default App;
